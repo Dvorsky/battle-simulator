722c9325060bc4b87239901068255a0386f8c6c7
Dvorsky <dvorsky.andrija@gmail.com>
2018-01-24 04:33:51 +0100

    I created classes for BaseTroop and Human Troop that extends it

---
+++ src/Models/BaseTroop.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+
+class BaseTroop
+{
+    public $health, $str, $def, $fit;
+
+    /**
+     * basic stats to a troop model
+     */
+    public function set_stats()
+    {
+
+        $this->health = 100;
+        $this->str = rand(10, 20);
+        $this->def = rand(5, 10);
+        $this->fit = rand(70, 100);
+
+    }
+
+
+}
